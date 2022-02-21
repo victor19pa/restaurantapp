d2fdc454ed1d54254ca42b88a490d81c28583524
victor <vpinedaa@unah.hn>
2022-02-21 09:40:02 -0600

    mostrando productos c native base

--- views/Menu.js
+++ views/Menu.js
@@ -1,6 +1,8 @@
-import React, { useContext, useEffect } from 'react';
-import { Text } from 'react-native';
+import { Container, Content, List, ListItem, Separator, Thumbnail, Text, Left, Body } from 'native-base';
+import React, { Fragment, useContext, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import FirebaseContext from '../context/firebase/firebaseContext';
+import globalStyles from '../styles/global';
 
 const Menu = () => {
 
@@ -9,11 +11,43 @@ const Menu = () => {
 
     useEffect(() =>{
         obtenerProductos();
-        console.log(menu)
+        //console.log(menu)
     },[])
 
     return (  
-        <Text>Menu</Text>
+        <Container style={globalStyles.contenedor}>
+            <Content style={{backgroundColor: '#FFF'}}>
+                <List>
+                    {menu.map(platillo => {
+                        const { imagen, nombre, descripcion, categoria, precio, id } = platillo
+
+                        return(
+                            <Fragment key={id}>
+                                <ListItem 
+
+                                >
+                                    <Thumbnail
+                                        large
+                                        square
+                                        source={{uri: imagen}}
+                                    />
+                                    <Body>
+                                        <Text>{nombre}</Text>
+                                        <Text
+                                            note
+                                            numberOfLines={2}
+                                        >
+                                            {descripcion}
+                                        </Text>
+                                        <Text>Precio: L{precio}</Text>
+                                    </Body>
+                                </ListItem>
+                            </Fragment>
+                        )
+                    })}
+                </List>
+            </Content>
+        </Container>
     );
 }
  
