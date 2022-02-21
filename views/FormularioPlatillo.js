62635e4d98bb20cef85417295c8e2f01fd16b29a
victor <vpinedaa@unah.hn>
2022-02-21 15:37:19 -0600

    Calcular total

--- views/FormularioPlatillo.js
+++ views/FormularioPlatillo.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { Container, Content, Form, Icon, Input, Grid, Col, Button, Text } from 'native-base';
 import { useNavigation } from '@react-navigation/native'
@@ -6,21 +6,36 @@ import globalStyles from '../styles/global';
 import PedidoContext from '../context/pedidos/pedidosContext';
 
 const FormularioPlatillo = () => {
+
     //state para cantidad
     const [ cantidad, setCantidad ] = useState(1)
+    const [ total, setTotal ] = useState(0)
 
-    //almacenar cantidad via input
+    //context pedido
+    const { platillo } = useContext(PedidoContext)
+    const { precio } = platillo
+
+    //calcular catnidad a pagar no mas cargue el comp
+    useEffect(() => {
+        calcularTotal()
+    },[cantidad])
+
+    //Manejar cantidad via input
     const incrementarUno = () => {
          const nuevaCantidad = parseInt(cantidad) + 1;
          setCantidad(nuevaCantidad)
     };
-
     const decrementarUno = () => {
         if(cantidad > 1){
             const nuevaCantidad = parseInt(cantidad) - 1;
             setCantidad(nuevaCantidad)
         }   
-   };
+    };
+   //calcular total
+    const calcularTotal = () => {
+        const totalPagar = precio * cantidad;
+        setTotal(totalPagar)
+    };
     return (  
         <Container>
             <Content>
@@ -65,6 +80,7 @@ const FormularioPlatillo = () => {
                         </Col>
                         
                     </Grid>
+                    <Text style={globalStyles.cantidad}>Subtotal: L{total}</Text>
                 </Form>
             </Content>
         </Container>
