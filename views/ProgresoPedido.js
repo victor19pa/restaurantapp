71334dba95470218af0b2cfd8a514596540f4a77
victor <vpinedaa@unah.hn>
2022-02-21 20:00:16 -0600

    mostrar tiempo entrega

--- views/ProgresoPedido.js
+++ views/ProgresoPedido.js
@@ -4,13 +4,25 @@ import React, {useContext, useEffect, useState} from 'react';
 import { StyleSheet, View } from 'react-native';
 import PedidoContext from '../context/pedidos/pedidosContext';
 import globalStyles from '../styles/global';
-
+import firebase from '../firebase'
 const ProgresoPedido = () => {
     //con este id podedmos consultar la base de firebase
     const { idpedido } = useContext(PedidoContext);
 
+    const [ tiempo, setTiempo ] = useState(0)
+    
+    useEffect(() => {
+        const obtenerProducto = () => {
+            firebase.db.collection('ordenes')
+                .doc(idpedido)
+                .onSnapshot(function(doc){
+                    setTiempo(doc.data().tiempoentrega)
+                })   
+        }
+        obtenerProducto()
+    },[])
     return (  
-        <Text>{idpedido}</Text>
+        <Text>{tiempo}</Text>
     );
 }
  
