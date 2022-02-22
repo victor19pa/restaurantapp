c4da85efcfff6fd5a53d0ce1ace31c4aa2d0cb18
victor <vpinedaa@unah.hn>
2022-02-22 08:56:23 -0600

    componente Countdown, completando

--- views/ProgresoPedido.js
+++ views/ProgresoPedido.js
@@ -5,6 +5,8 @@ import { StyleSheet, View } from 'react-native';
 import PedidoContext from '../context/pedidos/pedidosContext';
 import globalStyles from '../styles/global';
 import firebase from '../firebase'
+import Countdown from 'react-countdown'
+
 const ProgresoPedido = () => {
     //con este id podedmos consultar la base de firebase
     const { idpedido } = useContext(PedidoContext);
@@ -21,9 +23,48 @@ const ProgresoPedido = () => {
         }
         obtenerProducto()
     },[])
+
+    //muesstra countdown
+    const renderer = ({minutes, seconds}) => {
+        
+
+        return(
+            <Text style={styles.tiempo}>{minutes}:{seconds}</Text>
+        )
+    };
+
     return (  
-        <Text>{tiempo}</Text>
+        <Container style={globalStyles.contenedor}>
+            <View style={[globalStyles.contenido, {marginTop: 50}]}>
+                { tiempo === 0 && (
+                    <>
+                        <Text style={{textAlign: 'center'}}>Hemos recibido tu orden...</Text>
+                        <Text style={{textAlign: 'center'}}>Estamos calculando el tiempo de entrega</Text>
+                    </>
+                )}
+                {tiempo > 0 && (
+                    <>
+                        <Text style={{textAlign: 'center'}}>Su orden estara lista en </Text>
+                        <Text>
+                            <Countdown 
+                                date={Date.now() + tiempo * 60000}
+                                renderer={renderer}
+                            />
+                        </Text>
+                    </>
+                )}
+            </View>
+        </Container>
     );
 }
+
+const styles = StyleSheet.create({
+    tiempo:{
+        marginBottom: 20,
+        marginTop: 30,
+        fontSize: 60,
+        textAlign: 'center'
+    }
+})
  
 export default ProgresoPedido;
